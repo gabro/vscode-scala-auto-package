3cd42d87f9a6b8332c96efbdd01956d7b4afc4a9
Gabriele Petronella <gabriele@buildo.io>
2019-01-13 11:23:57 +0100

    Remove debug message

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import { Position } from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  vscode.window.showInformationMessage("Activated");
   const fileSystemWatcher = vscode.workspace.createFileSystemWatcher(
     "**/*.scala",
     false,
